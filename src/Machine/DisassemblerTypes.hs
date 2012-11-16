7b77a5908e801665cd3c575c60f9eb5ca3ff0c63
B. Scott Michel <bscottm@ieee.org>
2012-11-16 13:55:07 -0800

    hack_121116
    Giving fclabels (lenses) a shot to see how that makes things easier, as a
    lot of people claim.

--- src/Machine/DisassemblerTypes.hs
+++ src/Machine/DisassemblerTypes.hs
@@ -1,15 +1,24 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
 -- | Basic types for all machine disassemblers
 module Machine.DisassemblerTypes
   ( DisasmElement(..)
-  , Disassembly
+  , Disassembly(..)
   , Disassembler(..)
-  , NullPseudoOp
+  , NullPseudoOp(..)
   ) where
 
+import Control.Category
+import Data.Label
+import Prelude hiding ((.), id)
+
+import Data.Map (Map)
 import Data.Sequence (Seq)
 import Data.Vector.Unboxed (Vector)
+import Data.ByteString (ByteString)
 
+-- | 
 data DisasmElement addrType wordType instructionType pseudoOptype where
   DisasmInst :: addrType
              -> Vector wordType
@@ -18,9 +27,20 @@ data DisasmElement addrType wordType instructionType pseudoOptype where
   DisasmPseudo :: pseudoOpType
                -> DisasmElement addrType wordType instructionType pseudoOpType
   
--- | A 'Disassembly' produces sequence of tuples, where each tuple corresponds to addresses, words corresponding to
--- the disassembled instruction, and the disassembled instruction.
-type Disassembly addrType wordType instructionType pseudoOpType = Seq (DisasmElement addrType wordType instructionType pseudoOpType)
+-- | A 'Disassembly' encapsulates the disassembler state. 
+data Disassembly addrType wordType instructionType pseudoOpType =
+  Disassembly
+  { _labelNum :: Int                                                                -- ^ A general-purpose label counter, useful
+                                                                                    -- for local labels, e.g., "L1", "L2", etc.,
+                                                                                    -- that are inserted into the symbol table.
+  , _symbolTab :: Map addrType ByteString                                           -- ^ The symbol table for address references in
+                                                                                    -- 'disasmSeq'
+  , _disasmSeq :: Seq (DisasmElement addrType wordType instructionType pseudoOpType) -- ^ The sequence of tuples, each of which is
+                                                                                    -- an address, words corresponding to the
+                                                                                    -- disassembled instruction, and the
+                                                                                    -- disassembled instruction.
+
+  }
 
 -- |  The 'Disassembler' provides a class for all disassemblers. Note that this
 class Disassembler addrType dispType wordType instructionType pseudoOpType where
@@ -33,3 +53,5 @@ class Disassembler addrType dispType wordType instructionType pseudoOpType where
               -> Disassembly addrType wordType instructionType pseudoOpType -- ^ The resulting disassembly sequence
 
 data NullPseudoOp = NullPseudoOp
+
+mkLabels [ ''Disassembly ]
