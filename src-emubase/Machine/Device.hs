f395a369b81df21b1a3aeaa8ba4ed4acd9dd43fb
B. Scott Michel <bscottm@ieee.org>
2017-01-12 23:17:31 -0800

    device writer.

--- src-emubase/Machine/Device.hs
+++ src-emubase/Machine/Device.hs
@@ -12,11 +12,13 @@ module Machine.Device
     , DeviceThings(..)
     , DeviceIO(..)
     , DevReaderFunc
+    , DevWriterFunc
     , deviceRead
+    , deviceWrite
     ) where
 
 import           Control.Arrow              (second)
-import           Control.Monad.State.Strict (State, runState)
+import           Control.Monad.State.Strict (State, runState, execState)
 
 -- | The emulated device type.
 data Device addrType wordType where
@@ -42,7 +44,11 @@ class (Monoid dev) => DeviceThings dev where
 -- | Type signature for device reader functions
 type DevReaderFunc dev addrType wordType = addrType -> State dev wordType
 
--- | Device-specific operations that depend on an address and a word type.
+-- | Type signature for device writer functions
+type DevWriterFunc dev addrType wordType = addrType -> wordType -> State dev wordType
+
+-- | Device-specific operations that depend on an address and a word type. This type class is necessary because the
+-- actual device `dev` in the `Device` type is existential; the type class unboxes `dev`.
 class (DeviceThings dev) => DeviceIO dev addrType wordType where
   -- | Device reader function: Read a word (`wordType`) from the device at an address (`addrType`), potentially
   -- changing the device's state. Returns a '(word, newDeviceState)' pair, which is the same result as a `State`
@@ -51,6 +57,12 @@ class (DeviceThings dev) => DeviceIO dev addrType wordType where
   -- __Note__: Do not call this function directly, primarily because it is a `State` computation. Use `deviceRead`
   -- instead.
   deviceReader :: DevReaderFunc dev addrType wordType
+  -- | Device writer function: Write a word (`wordType`) to the device at an address (`addrType`), which definitely
+  -- changes the device's state. Returns the new device state.
+  --
+  -- __Note__: Do not call this function directly, primarily because it is a `State` computation. Use `deviceWrite`
+  -- instead.
+  deviceWriter :: DevWriterFunc dev addrType wordType
 
 -- ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
 -- Memory-mapped devices:
@@ -65,3 +77,11 @@ deviceRead :: addrType
            -> (wordType, Device addrType wordType)
            -- ^ Value/word read and updated device state pair
 deviceRead addr (Device dev) = second Device (runState (deviceReader addr) dev)
+
+-- | Write a word to a device, returning the new device state as its result. This function invokes `execState`
+-- to run the state forward, since only the state is important.
+deviceWrite :: addrType
+            -> wordType
+            -> Device addrType wordType
+            -> Device addrType wordType
+deviceWrite addr word (Device dev) = Device (execState (deviceWriter addr word) dev)
