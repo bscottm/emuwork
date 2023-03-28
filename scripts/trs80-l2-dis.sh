c8e482121cb8b6fe01c968774eee5dad0d884b5b
B. Scott Michel <scooter.phd@gmail.com>
2023-03-28 14:05:22 -0700

    renormalized line endings

---
+++ scripts/trs80-l2-dis.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# stack exec trs80l2rom -- --format=raw trs80/roms/level2.rom
+stack exec emuwork -- --system=trs80-model-i disasm --format=raw --image=trs80/roms/level2.rom --mem=48 --guidance=guidance.yaml
